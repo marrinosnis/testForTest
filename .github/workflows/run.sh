1ac3cd2f4674482c822e41ad86850ba2353d52b2
marinos <marinosnisiotis@gmail.com>
2022-11-03 11:30:10 +0200

    change again the run.sh jenkins

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -5,7 +5,7 @@ echo "The repository that is currently running is: $GITHUB_REPOSITORY"
 
 COMMIT_MESSAGE=$(git log -1 HEAD --pretty=format:%s)
 
-if COMMIT_MESSAGE | grep -i -q -e "jenkins" -e "jenk" -e "kins";
+if "$COMMIT_MESSAGE" | grep -i -q -e "jenkins" -e "jenk" -e "kins";
 then
     echo "Found keyword"
 else
