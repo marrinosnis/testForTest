bc3877724ef1d7b725544242ea1722be4a8cfcbb
marinos <marinosnisiotis@gmail.com>
2022-11-04 23:56:47 +0200

    marinos deks test jenkins

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ COMMIT_MESSAGE=$(git log -1 HEAD --pretty=format:%s)  #each time takes the most 
 jenk='jenkins'
 # echo $COMMIT_MESSAGE
 echo
-if "$COMMIT_MESSAGE" | grep -i -q "jenkins"
+if echo "$COMMIT_MESSAGE" | grep -i -q -e "jenkins" -e "jenk" -e "kins";
 then
     echo "Found keyword"   #here will be added the jenkins job-pipeline if the word jenkins is found
 else
