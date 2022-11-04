df490188d3cea8a97e356dae8f9d940229b30781
marinos <marinosnisiotis@gmail.com>
2022-11-04 11:31:47 +0200

    check the jenkins

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -3,11 +3,11 @@ set -e
 
 echo "The repository that is currently running is: $GITHUB_REPOSITORY"
 
-COMMIT_MESSAGE=$(git log -1 HEAD --pretty=format:%s)
+COMMIT_MESSAGE=$(git log -1 HEAD --pretty=format:%s)  #each time takes the most recent commit after push
 
-if "$COMMIT_MESSAGE" | grep -i -q -e "jenkins" -e "jenk" -e "kins";
+if [[ $COMMIT_MESSAGE =~ "jenkins" ]] ; 
 then
-    echo "Found keyword"
+    echo "Found keyword"   #here will be added the jenkins job-pipeline if the word jenkins is found
 else
     echo "Nothing to process Marinos"
 fi
