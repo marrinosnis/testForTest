0771dbd7d6b79df009f670fdd4b45c28475ef9f2
marinos <marinosnisiotis@gmail.com>
2022-11-04 11:41:53 +0200

    jenkins check2

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -4,8 +4,10 @@ set -e
 echo "The repository that is currently running is: $GITHUB_REPOSITORY"
 
 COMMIT_MESSAGE=$(git log -1 HEAD --pretty=format:%s)  #each time takes the most recent commit after push
-
-if [[ $COMMIT_MESSAGE =~ "jenkins" ]] ; 
+echo
+ehco COMMIT_MESSAGE
+echo
+if [[ $COMMIT_MESSAGE == *jenkins* ]] ; 
 then
     echo "Found keyword"   #here will be added the jenkins job-pipeline if the word jenkins is found
 else
