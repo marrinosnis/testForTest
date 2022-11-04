d152dc8afa250b108b44f3d44417ed0ac7f6f9c6
marinos <marinosnisiotis@gmail.com>
2022-11-05 00:19:39 +0200

    change the path of the script jenkins

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -4,7 +4,7 @@ set -e
 echo "The repository that is currently running is: $GITHUB_REPOSITORY"
 
 COMMIT_MESSAGE=$(git log -1 HEAD --pretty=format:%s)  #each time takes the most recent commit after push
-jenk='jenkins'
+
 # echo $COMMIT_MESSAGE
 echo
 if echo "$COMMIT_MESSAGE" | grep -i -q -e "jenkins" -e "jenk" -e "kins";
