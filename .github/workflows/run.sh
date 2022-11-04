a41d01c0b021bb4ed184134d36f250de8aa6022a
marinos <marinosnisiotis@gmail.com>
2022-11-04 11:44:22 +0200

    jenkins check4

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -5,7 +5,7 @@ echo "The repository that is currently running is: $GITHUB_REPOSITORY"
 
 COMMIT_MESSAGE=$(git log -1 HEAD --pretty=format:%s)  #each time takes the most recent commit after push
 echo
-ehco $COMMIT_MESSAGE
+echo $COMMIT_MESSAGE
 echo
 if [[ $COMMIT_MESSAGE == *jenkins* ]] ; 
 then
