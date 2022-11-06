6011ab24eba1ab45b4911dd07dd8cba40e12a212
marinos <marinosnisiotis@gmail.com>
2022-11-06 12:30:30 +0200

    push again

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -2,19 +2,22 @@
 set -e
 
 echo "The repository that is currently running is: $GITHUB_REF_NAME"
+echo
 
-EPIC_CHECK=${GITHUB_REF_NAME: -5}
+if [[ $GITHUB_REF_NAME == *_epic ]];
+then
+    echo "The branch is EPIC"
+else
+    echo "The brach is about to RUN"
+fi
 
-echo "The value of the EPIC_CHECK is" 
-echo $EPIC_CHECK
-
+echo
 COMMIT_MESSAGE=$(git log -1 HEAD --pretty=format:%s)  #each time takes the most recent commit after push
 
-# echo $COMMIT_MESSAGE
 echo
-if echo "$COMMIT_MESSAGE" | grep -i -q -e "jenkins" -e "jenk" -e "kins"; #should change that, with the shell script testing [[ .. ]]
+if echo "$COMMIT_MESSAGE" | grep -i -q -e "jenkins" -e "jenk" -e "kins"; #TODO: should change that, with the shell script testing [[ .. ]]
 then
-    echo "The jenkins job should be triggered"   #here will be added the jenkins job-pipeline if the word jenkins is found
+    echo "The jenkins job should be triggered"   #TODO: add here will be added the jenkins job-pipeline if the word jenkins is found
 else
     echo "No job should run-trigerred"
 fi
