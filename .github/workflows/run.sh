fa245b755ec18fd03f0a6c2ef0a2975bfe37614b
marinos <marinosnisiotis@gmail.com>
2022-11-15 13:42:32 +0200

    appears better jenkins

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -e
 
-echo "The repository that is currently running is: $GITHUB_REF_NAME"
+echo "The branch that is currently running is: $GITHUB_REF_NAME"
 
 if [[ $GITHUB_REF_NAME == *_epic ]]; then 
     echo "The branch is EPIC"  # if the the name of branch ends in '_epic' I DO NOT want to triger the jenkins pipeline
 else
-    #echo "The branch is NOT EPIC, and is about to run"
+    echo "The branch is NOT EPIC, and is about to run"
+    echo; echo;
     COMMIT_MESSAGE=$(git log -1 HEAD --pretty=format:%s)  #each time takes the most recent commit after push
 
     if echo "$COMMIT_MESSAGE" | grep -i -q -e "jenkins" -e "jenk" -e "kins" #TODO: should change that, with the shell script testing [[ .. ]]
